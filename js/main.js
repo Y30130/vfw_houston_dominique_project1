ba67f7a0566fd54924431fbdfa3a2511a2318a90
Dominique Houston <dominiquehouston@gmail.com>
2013-06-12 21:25:44 -0700

    Addition of final functions

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ window.addEventListener("DOMContentLoaded", function(){
     
     // Find value of selcted radio button 
     function getSelectedRadio(){
-      var radio = document.forms[0].workCompleted;
+      var radio = document.forms[0].readTitle;
       for(var i=0; i<radio.length; i++){
          if(radio[i].checked){
          completedValue = radio[i].value;
@@ -48,6 +48,28 @@ window.addEventListener("DOMContentLoaded", function(){
 	    }
     }
     
+    // Toggle Controls
+    function toggleControls(n){
+    	switch(n) {
+	    	case "on":
+	    		$('bookeepForm').style.display = "none";
+	    		$('clearLink').style.display = "inline";
+	    		$('displayLink').style.display = "none";
+	    		$('addNew').style.display = "inline";
+	    		break;
+	    	case "off":
+	    		$('bookeepForm').style.display = "block";
+	    		$('clearLink').style.display = "inline";
+	    		$('displayLink').style.display = "inline";
+	    		$('addNew').style.display = "none";
+	    		$('items').style.dispplay = "none";
+	    		break;
+	    	
+	    	default:
+	    		return false;
+    	}
+    }
+    
     
     // Save Our Data
     function storeData () {
@@ -57,35 +79,78 @@ window.addEventListener("DOMContentLoaded", function(){
 	    getSelectedRadio();
 	    getCheckboxValue();
 	    var item 			= {};
-	    	item.platform				= ["Platform:", $(platforms).value];
-	    	item.workTitle				= ["Title:", $(workTitle).value];
-	    	item.workAuthorName			= ["Author:", $(workAuthorName).value];
-	    	item.workAuthorPublished 	= ["Year Published:", $(workAuthorPublished).value];
-	    	item.workAuthorUrl 			= ["Work Author URL:", $(workAuthorUrl).value];
+	    	item.workTitle				= ["Title:", $('workTitle').value];
+	    	item.workAuthorName			= ["Author:", $('workAuthorName').value];
+	    	item.workAuthorPublished 	= ["Year Published:", $('workYearPublished').value];
+	    	item.workAuthorUrl 			= ["Work Author URL:", $('workUrl').value];
 	    	item.workCompleted			= ["Finished Reading:", completedValue];
 	    	item.favorite 				= ["Reason for Reading:", favoriteValue];
-	    	item.notes 					= ["Notes:",];
-	    	item.readStart		    	= ["Read Start Date:",];
-	    	item.readFinish 			= ["Read Finish Date:",];
-	    	item.stars 					= ["Rating:",];
+	    	item.platform				= ["Platform:", $('platforms').value];
+	    	item.notes 					= ["Notes:",$('notes').value];
+	    	item.readStart		    	= ["Read Start Date:",$('readStart').value];
+	    	item.readFinish 			= ["Read Finish Date:",$('readFinish').value];
+	    	item.stars 					= ["Rating:",$('stars').value];
 	    	// Save data into Local Storage: Use Stringify to convert our object to a string
 	    	localStorage.setItem(id, JSON.stringify(item));
 	    	alert("Bookept!")
-	    	
+    }
+    
+    // Display our Data on the Front-End
+       function displayData(){
+       toggleControls("on");
+       if(localStorage.length === 0) {
+	       alert("There is no data in Local Storage");
+       }
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
+		for(var i=0, len=localStorage.length; i<len;i++) {
+			var makeLi = document.createElement('li');
+			makeList.appendChild(makeLi);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			    //convert string back to object
+		         var infoObj = JSON.parse(value);
+		         var createSubList = document.createElement('ul');
+		         makeLi .appendChild(createSubList);
+		         for(var y in infoObj){
+		            var createSubli = document.createElement('li');
+		            createSubList.appendChild(createSubli);
+		            var optSubText = infoObj[y] [0] +" "+ infoObj[y] [1];
+		            createSubli.innerHTML = optSubText
+		         }
+		         
+		      }
+		    
+		   }
+
+    // Clear Data
+    function clearData() {
+	    if(localStorage.length === 0){
+	    alert("There is no data to clear.");		    
+	    }else {
+		    localStorage.clear();
+		    alert("All titles have been removed.");
+		    window.location.reload();
+		    return false;
+	    }
     }
     
     
 	// Variable defaults
 	var readPlatform = ["-- Choose A Platform --", "Book - Paperback", "Book - Hardcover", "Book - Other", "Digital - Personal Computer", "Digital - Tablet/eReader", "Digital - Other"], 
 	completedValue, 
-	favoriteValue = No;
+	favoriteValue = "No";
     makePlatforms();
 
 // Set Link & Submit Click Events
-	/* var displayLink = $('displayLink');
-	displayLink.addEventListener("click", getData);
+	 var displayLink = $('displayLink');
+	displayLink.addEventListener("click", displayData);
 	var clearLink = $('clearLink');
-	clearLink.addEventListener("click",clearLocal); */
+	clearLink.addEventListener("click",clearData);
 	var save = $('submit');
 	save.addEventListener("click", storeData);
 
