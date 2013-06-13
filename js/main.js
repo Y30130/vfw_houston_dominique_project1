cfcb255631cf924ce81eae5e15b820a5ca478698
Dominique Houston <dominiquehouston@gmail.com>
2013-06-12 17:32:19 -0700

    Javascript Setup

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@
 // Assignment 2
 //
 
+// Wait until the DOM is ready.
+window.addEventListener("DOMContentLoaded", function(){
+
+
+	// getElementByID Function
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+
+// Variable defaults
+	var readingPlatform = ["--Select a Platform--", "Digital", "Book"];
+
+// Set Link & Submit Click Events
+	var displayLink = $('displayLink');
+	displayLink.addEventListener("click", getData);
+	var clearLink = $('clearLink');
+	clearLink.addEventListener("click",clearLocal);
+	var save = $('submit');
+	save.addEventListener("click", storeData);
+
+
+});
+
